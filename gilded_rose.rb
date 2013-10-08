b13a556287cc6ce38e2cc7aaeb8abf782e24f2b6
Heliosmaster <helios89@gmail.com>
2013-10-08 12:27:46 +0200

    creation of update_item method, using awesome_print gem and printouts

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,65 +1,74 @@
-def update_quality
-  0.upto(Items.length-1) do |i|
-    if (Items[i].name != "Aged Brie" && Items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-      if (Items[i].quality > 0)
-        if (Items[i].name != "Sulfuras, Hand of Ragnaros")
-          Items[i].quality = Items[i].quality - 1
-        end
+require "awesome_print"
+
+def update_item(item)
+  if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
+    if (item.quality > 0)
+      if (item.name != "Sulfuras, Hand of Ragnaros")
+        item.quality = item.quality - 1
       end
-    else
-      if (Items[i].quality < 50)
-        Items[i].quality = Items[i].quality + 1
+    end
+  else
+    if (item.quality < 50)
+      item.quality = item.quality + 1
 
-        if (Items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-          if (Items[i].sell_in < 11)
-            if (Items[i].quality < 50)
-              Items[i].quality = Items[i].quality + 1
-            end
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+        if (item.sell_in < 11)
+          if (item.quality < 50)
+            item.quality = item.quality + 1
           end
+        end
 
-          if (Items[i].sell_in < 6)
-            if (Items[i].quality < 50)
-              Items[i].quality = Items[i].quality + 1
-            end
+        if (item.sell_in < 6)
+          if (item.quality < 50)
+            item.quality = item.quality + 1
           end
         end
       end
     end
+  end
 
-    if (Items[i].name != "Sulfuras, Hand of Ragnaros")
-      Items[i].sell_in = Items[i].sell_in - 1
-    end
+  if (item.name != "Sulfuras, Hand of Ragnaros")
+    item.sell_in = item.sell_in - 1
+  end
 
-    if (Items[i].sell_in < 0)
-      if (Items[i].name != "Aged Brie")
-        if (Items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-          if (Items[i].quality > 0)
-            if (Items[i].name != "Sulfuras, Hand of Ragnaros")
-              Items[i].quality = Items[i].quality - 1
-            end
+  if (item.sell_in < 0)
+    if (item.name != "Aged Brie")
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+        if (item.quality > 0)
+          if (item.name != "Sulfuras, Hand of Ragnaros")
+            item.quality = item.quality - 1
           end
-        else
-          Items[i].quality = Items[i].quality - Items[i].quality
         end
       else
-        if (Items[i].quality < 50)
-          Items[i].quality = Items[i].quality + 1
-        end
+        item.quality = item.quality - item.quality
+      end
+    else
+      if (item.quality < 50)
+        item.quality = item.quality + 1
       end
     end
   end
 end
 
-# The code below belongs to the goblin in the corner
-# do not change, or he will insta-rage and one-shot you
+def update_quality
+  0.upto(Items.length-1) do |i|
+    update_item(Items[i])
+  end
+end
+  # The code below belongs to the goblin in the corner
+  # do not change, or he will insta-rage and one-shot you
 
-Item = Struct.new :name, :sell_in, :quality
+  Item = Struct.new :name, :sell_in, :quality
 
-Items = [
-  Item.new("+5 Dexterity Vest", 10, 20),
-  Item.new("Aged Brie", 2, 0),
-  Item.new("Elixir of the Mongoose", 5, 7),
-  Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-  Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  Item.new("Conjured Mana Cake", 3, 6)
-]
+  Items = [
+    Item.new("+5 Dexterity Vest", 10, 20),
+    Item.new("Aged Brie", 2, 0),
+    Item.new("Elixir of the Mongoose", 5, 7),
+    Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+    Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    Item.new("Conjured Mana Cake", 3, 6)
+  ]
+
+##############################################
+100.times { update_quality }
+ap Items
